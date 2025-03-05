60b1b77b0d603ee34d2c260bbf08b2f8dc94ff13
Markó Kevin <markokevin@ktch.hu>
2025-03-05 13:44:15 +0100

    UserControll

--- database/migrations/2025_02_26_133918_create_customers_table.php
+++ database/migrations/2025_02_26_133918_create_customers_table.php
@@ -3,25 +3,20 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Customer;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->foregnid();
+            $table->id();
             $table->integer('people_amount');
             $table->integer('table_id');
             
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('customers');
